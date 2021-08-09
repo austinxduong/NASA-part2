cd5a310e498d1729a32d42d551e0908990a24ceb
austinxduong <axduong2@gmail.com>
2021-08-09 10:39:58 -0700

    create 1st failing test: red, green, refactor

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -3,8 +3,21 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('Planet routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a planet via. POST', async () => {
+    const  res = await request(app)
+      .post('/api/v1/planet')
+      .send({
+        name: 'Venus',
+        namesake: 'Roman Goddess of Love',
+        lengthOfYear: '225 Earth Days',
+        moons: '0',
+        zodiacs: 'Taurus, Libra'
+
+      })
+  }
 });
