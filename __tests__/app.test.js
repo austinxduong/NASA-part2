4c2e3f83afc35f2809f2f726d4b85fac75bbedae
austinxduong <axduong2@gmail.com>
2021-08-13 21:38:03 -0700

    second failing test findAll route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Planet from '../lib/models/Planet.js';
+import Planet from '../lib/models/Planet.js';
 
 // import Planet from '../lib/models/Planet.js';
 // remember, numerical values are not be wrapped in strings
@@ -35,7 +35,37 @@ describe('Planet routes', () => {
       planetType: 'Terrestrial',
     });
   });
+
+  it('finds all planet via GET', async () => {
+
+    const test1 = await Planet.insert({
+      name: 'Venus',
+      moons: 0,
+      image: 'venus.jpg'
+    });
+
+    const test2 = await Planet.insert({
+      name: 'Jupiter',
+      moons: 53,
+      image: 'jupiter.jpg'
+    });
+
+    const test3 = await Planet.insert({
+      name: 'Mercury',
+      moons: 0,
+      image: 'mercury.jpg'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/planets');
+
+    expect(res.body).toEqual([test1, test2, test3]);
+  });
+
 });
+
+
+
 
 
 
